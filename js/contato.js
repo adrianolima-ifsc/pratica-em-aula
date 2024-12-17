b355cedc8b2c4de027a88ea40ed577e391562332
Adriano Lima <adriano.lima@ifsc.edu.br>
2024-12-17 18:39:54 -0300

    Aula 12/12

---
+++ js/contato.js
@@ -0,0 +1,29 @@
+const formulario = document.querySelector('.contato__formulario');
+// const botao = document.querySelector('')
+
+const contatos = [];
+
+formulario.addEventListener("submit", evento => {
+    evento.preventDefault();
+
+    let nome = document.querySelector('#input-nome').value;
+    let email = document.querySelector('#input-email').value;
+    let periodo = document.querySelector('#input-periodo').value;
+    let mensagem = document.querySelector('#input-mensagem').value;
+
+    const aviso = document.querySelector('.contato__texto');
+    if ( !nome || !email || !periodo || !mensagem ) {
+        aviso.textContent = "Preencha todos os campos";
+        aviso.classList.add('cuidado');
+        return
+    }
+
+    const contato = { nome, email, periodo, mensagem };
+    contatos.push(contato);
+
+    console.log(contatos);
+
+    evento.target.reset();
+    aviso.textContent = "Responderemos depois da pescaria."
+    aviso.classList.remove('cuidado');
+});
